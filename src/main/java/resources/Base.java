3169fb613418c6fa193a002b28d1cfbfbbc6cf9f
Vlad Kononov <vladimirkon@gmail.com>
2020-06-08 16:32:33 -0700

    add Base.java and browser drivers

---
+++ src/main/java/resources/Base.java
@@ -0,0 +1,69 @@
+package resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Base {
+
+	public static WebDriver driver;
+	public Properties prop;
+
+	public WebDriver initializeDriver() throws IOException {
+
+		// to load the data from file to properties
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "\\src\\main\\java\\resources\\data.properties");
+		prop.load(fis);
+
+		/*
+		 * sending parameter using mvn command mvn test -Dbrowser=firefox
+		 */
+		String browserName = System.getProperty("browser");
+		/*
+		 * using data.properties file to set the browser
+		 */
+//		 String browserName = prop.getProperty("browser");
+
+		System.out.println(browserName);
+
+		if (browserName.contains("chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\main\\java\\resources\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			if(browserName.contains("headless")) {
+				options.addArguments("headless");
+			}
+				driver = new ChromeDriver(options);
+		} else if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\main\\java\\resources\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("IE")) {
+			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "\\src\\main\\java\\resources\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		return driver;
+	}
+
+	public String getScreenShotPath(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String destinationFile = System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
+		FileUtils.copyFile(source, new File(destinationFile));
+		return destinationFile;
+	}
+	
+}
