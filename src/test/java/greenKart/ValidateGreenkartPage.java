29da7c9b0987a827e8adc34c80f7bd340cd53179
Vlad Kononov <vladimirkon@gmail.com>
2020-06-09 00:36:17 -0700

    added Base and ValidateGreenkartPage tests

---
+++ src/test/java/greenKart/ValidateGreenkartPage.java
@@ -0,0 +1,51 @@
+package greenKart;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+import pageObjects.GreenkartPage;
+import resources.Base;
+
+public class ValidateGreenkartPage extends Base {
+	public WebDriver driver;
+
+	private static Logger log = LogManager.getLogger(Base.class.getName());
+	public GreenkartPage g;
+	
+	@BeforeTest
+	public void initialize() throws IOException {
+		driver = initializeDriver();
+		driver.get(prop.getProperty("url"));
+		g = new GreenkartPage(driver);
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+		driver = null;
+	}
+	
+	@Test(enabled=true)
+	public void validateTitle() {
+		System.out.println(g.getLogo().getText());
+		Assert.assertTrue(g.getLogo().isDisplayed(), "Logo is dislpayed");
+		log.info("GreenKart logo is displayed");
+	}
+	
+	@Test
+	public void validateOffer() {
+		System.out.println(g.getOffer().getText());
+		Assert.assertTrue(g.getOffer().isDisplayed(), "Offer is displayed");
+		g.getOffer().click();
+		log.info("Offer link is clickable");
+		
+	}
+}
