ba3d107e95e473ab799031c3d694a47929130a52
Vlad Kononov <vladimirkon@gmail.com>
2020-06-10 19:52:50 -0700

    added elements to GreenkartPage; added more tests

--- src/main/java/pageObjects/GreenkartPage.java
+++ src/main/java/pageObjects/GreenkartPage.java
@@ -1,5 +1,7 @@
 package pageObjects;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +13,11 @@ public class GreenkartPage {
 	private By logo = By.xpath("//div[@class='brand greenLogo']");
 	private By search = By.cssSelector("form input");
 	private By searchButton = By.cssSelector("button.search-button");
+	private By topDeals = By.xpath("//*[contains(text(),'Top Deals')]");
+	private By flightBooking = By.xpath("//*[contains(text(),'Flight Booking')]");
+	private By cart = By.cssSelector("a[class='cart-icon']");
+	private By products = By.cssSelector("div[class='product']");
+	private By productNames = By.cssSelector("h4[class='product-name']");
 	
 	
 	public GreenkartPage(WebDriver driver) {
@@ -32,5 +39,26 @@ public class GreenkartPage {
 	public WebElement getSearchButton() {
 		return driver.findElement(searchButton);
 	}
+	
+	public WebElement getTopDeals() {
+		return driver.findElement(topDeals);
+	}
+
+	public WebElement getFlightBooking() {
+		return driver.findElement(flightBooking);
+	}
+	
+	public WebElement cart() {
+		return driver.findElement(cart);
+	}
+
+	public List<WebElement> getProducts() {
+		return driver.findElements(products);
+	}
+	
+	public List<WebElement> getProductNames() {
+		return driver.findElements(productNames);
+	}
 
 }
+
--- src/test/java/greenKart/ValidateGreenkartPage.java
+++ src/test/java/greenKart/ValidateGreenkartPage.java
@@ -1,10 +1,12 @@
 package greenKart;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -49,4 +51,38 @@ public class ValidateGreenkartPage extends Base {
 		log.info("Offer link is clickable");
 		
 	}
+	
+	@Test
+	public void validateTopDeals() {
+		System.out.println(g.getTopDeals().getText());
+		Assert.assertTrue(g.getTopDeals().isDisplayed(), "Top Deals is not displayed");
+		g.getTopDeals().click();
+		log.info("Top Deals clicked");
+	}
+	
+	@Test
+	public void valiateFlightBooking() {
+		System.out.println(g.getFlightBooking().getText());
+		Assert.assertTrue(g.getFlightBooking().isDisplayed(), "Flight Booking is not displayed");
+		g.getFlightBooking().click();
+		log.info("Flight Booking clicked");
+	}
+	
+	@Test
+	public void valiateCart() {
+		System.out.println(g.cart().getText());
+		Assert.assertTrue(g.cart().isDisplayed(), "Cart is not displayed");
+		g.cart().click();
+		log.info("Cart clicked");
+	}
+	
+	@Test
+	public void validateProductNames() {
+		List<WebElement> products = g.getProductNames();
+		for(int i=0; i<g.getProducts().size(); i++) {
+			System.out.println(i+1 + " product is: " + products.get(i).getText());
+		}
+		log.info("There are " + products.size() + " products on the GreenKart page");
+	}
+	
 }
